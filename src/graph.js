57bd31c3bad211f8ec2c3402b8adef062872763f
gulshan kumar <gulhan@123>
2020-06-10 19:59:27 +0530

    Added: class Queue

--- src/graph.js
+++ src/graph.js
@@ -4,8 +4,25 @@
 // To => to 
 // Should return true.
 
-const bfs = function(pairs,source,target){
+class Queue {
+  constructor () {
+    this.queue = [];
+  }
+  enqueue(element) {
+    if (element && !this.queue.includes(element)) {
+      this.queue.push(element);
+    }
+  }
+  get dequeue() {
+    return this.queue.shift();
+  }
+  get isEmpty() {
+    return this.queue[0] ? false : true;
+  }
+}
+
+const bfs = function (pairs, source, target) {
   
 };
 
-module.exports = {bfs};
+module.exports = {Queue, bfs};
